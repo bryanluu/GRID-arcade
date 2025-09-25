13f6dd70e855f56c3a4da18685c313c74caf444e
Bryan Luu <loc.bryan.luu@gmail.com>
2025-09-25 11:37:08 -0700

    :recycle: make ExampleScene
    Using the Scene class, successfully compile and upload the example.

--- GRID/GRID.ino
+++ GRID/GRID.ino
@@ -32,73 +32,106 @@
 
 RGBmatrixPanel matrix(A, B, C, D, CLK, LAT, OE, false);
 
-void setup() {
+Scene * Scene::s_currentScene = nullptr;
 
-  matrix.begin();
+class ExampleScene : public Scene 
+{
+  public:
+    ExampleScene() : Scene()
+    {
+      m_name = "Example";
+    }
 
-  // draw a pixel in solid white
-  matrix.drawPixel(0, 0, matrix.Color333(7, 7, 7));
-  delay(500);
+    void start();
+    void run();
+};
 
-  // fix the screen with green
-  matrix.fillRect(0, 0, 32, 32, matrix.Color333(0, 7, 0));
-  delay(500);
+void ExampleScene::start()
+{
+    matrix.begin();
 
-  // draw a box in yellow
-  matrix.drawRect(0, 0, 32, 32, matrix.Color333(7, 7, 0));
-  delay(500);
+    // draw a pixel in solid white
+    matrix.drawPixel(0, 0, matrix.Color333(7, 7, 7));
+    delay(500);
 
-  // draw an 'X' in red
-  matrix.drawLine(0, 0, 31, 31, matrix.Color333(7, 0, 0));
-  matrix.drawLine(31, 0, 0, 31, matrix.Color333(7, 0, 0));
-  delay(500);
+    // fix the screen with green
+    matrix.fillRect(0, 0, 32, 32, matrix.Color333(0, 7, 0));
+    delay(500);
 
-  // draw a blue circle
-  matrix.drawCircle(10, 10, 10, matrix.Color333(0, 0, 7));
-  delay(500);
+    // draw a box in yellow
+    matrix.drawRect(0, 0, 32, 32, matrix.Color333(7, 7, 0));
+    delay(500);
 
-  // fill a violet circle
-  matrix.fillCircle(21, 21, 10, matrix.Color333(7, 0, 7));
-  delay(500);
+    // draw an 'X' in red
+    matrix.drawLine(0, 0, 31, 31, matrix.Color333(7, 0, 0));
+    matrix.drawLine(31, 0, 0, 31, matrix.Color333(7, 0, 0));
+    delay(500);
 
-  // fill the screen with 'black'
-  matrix.fillScreen(matrix.Color333(0, 0, 0));
+    // draw a blue circle
+    matrix.drawCircle(10, 10, 10, matrix.Color333(0, 0, 7));
+    delay(500);
 
-  // draw some text!
-  matrix.setCursor(1, 0);    // start at top left, with one pixel of spacing
-  matrix.setTextSize(1);     // size 1 == 8 pixels high
-  matrix.setTextWrap(false); // Don't wrap at end of line - will do ourselves
+    // fill a violet circle
+    matrix.fillCircle(21, 21, 10, matrix.Color333(7, 0, 7));
+    delay(500);
 
-  matrix.setTextColor(matrix.Color333(7,7,7));
-  matrix.println(" Ada");
-  matrix.println("fruit");
+    // fill the screen with 'black'
+    matrix.fillScreen(matrix.Color333(0, 0, 0));
 
-  // print each letter with a rainbow color
-  matrix.setTextColor(matrix.Color333(7,0,0));
-  matrix.print('3');
-  matrix.setTextColor(matrix.Color333(7,4,0));
-  matrix.print('2');
-  matrix.setTextColor(matrix.Color333(7,7,0));
-  matrix.print('x');
-  matrix.setTextColor(matrix.Color333(4,7,0));
-  matrix.print('3');
-  matrix.setTextColor(matrix.Color333(0,7,0));
-  matrix.println('2');
+    // draw some text!
+    matrix.setCursor(1, 0);    // start at top left, with one pixel of spacing
+    matrix.setTextSize(1);     // size 1 == 8 pixels high
+    matrix.setTextWrap(false); // Don't wrap at end of line - will do ourselves
 
-  matrix.setTextColor(matrix.Color333(0,7,7));
-  matrix.print('*');
-  matrix.setTextColor(matrix.Color333(0,4,7));
-  matrix.print('R');
-  matrix.setTextColor(matrix.Color333(0,0,7));
-  matrix.print('G');
-  matrix.setTextColor(matrix.Color333(4,0,7));
-  matrix.print('B');
-  matrix.setTextColor(matrix.Color333(7,0,4));
-  matrix.print('*');
+    matrix.setTextColor(matrix.Color333(7,7,7));
+    matrix.println(" Ada");
+    matrix.println("fruit");
 
-  // whew!
+    // print each letter with a rainbow color
+    matrix.setTextColor(matrix.Color333(7,0,0));
+    matrix.print('3');
+    matrix.setTextColor(matrix.Color333(7,4,0));
+    matrix.print('2');
+    matrix.setTextColor(matrix.Color333(7,7,0));
+    matrix.print('x');
+    matrix.setTextColor(matrix.Color333(4,7,0));
+    matrix.print('3');
+    matrix.setTextColor(matrix.Color333(0,7,0));
+    matrix.println('2');
+
+    matrix.setTextColor(matrix.Color333(0,7,7));
+    matrix.print('*');
+    matrix.setTextColor(matrix.Color333(0,4,7));
+    matrix.print('R');
+    matrix.setTextColor(matrix.Color333(0,0,7));
+    matrix.print('G');
+    matrix.setTextColor(matrix.Color333(4,0,7));
+    matrix.print('B');
+    matrix.setTextColor(matrix.Color333(7,0,4));
+    matrix.print('*');
+
+    // whew!
 }
 
-void loop() {
+void ExampleScene::run()
+{
   // Do nothing -- image doesn't change
+}
+
+ExampleScene exampleScene = ExampleScene();
+
+void setup() 
+{
+    Serial.begin(9600);
+    randomSeed(analogRead(0));
+    pinMode(0, INPUT_PULLUP);
+    matrix.begin();
+    matrix.setTextWrap(false);
+    matrix.setTextSize(1);
+    exampleScene.start();
+}
+
+void loop() 
+{
+   exampleScene.run();
 }
