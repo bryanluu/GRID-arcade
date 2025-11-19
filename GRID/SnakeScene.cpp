1ed7f953b5f88c1f16a7804beecc7de1bc4b6f34
Bryan Luu <loc.bryan.luu@gmail.com>
2025-11-18 16:54:23 -0800

    :sparkles: spawn+draw Snake at random position
    For now, just spawn a snake at a random position. To draw, just traverse
    through segments and draw each node.

--- GRID/SnakeScene.cpp
+++ GRID/SnakeScene.cpp
@@ -1,4 +1,68 @@
 #include "SnakeScene.h"
+#include "Colors.h"
+#include "Helpers.h"
+
+Snake::Snake(int startX, int startY)
+    : length_(3), direction_(Right)
+{
+    head_ = new Node(startX, startY);
+    Node *second = new Node(startX - 1, startY);
+    Node *third = new Node(startX - 2, startY);
+    head_->next = second;
+    second->next = third;
+    tail_ = third;
+}
+
+Snake::~Snake()
+{
+    Node *current = head_;
+    while (current)
+    {
+        Node *next = current->next;
+        delete current;
+        current = next;
+    }
+}
+
+int Snake::getHeadX() const
+{
+    return head_->x;
+}
+
+int Snake::getHeadY() const
+{
+    return head_->y;
+}
+
+Snake::Direction Snake::getDirection() const
+{
+    return direction_;
+}
+
+void Snake::setDirection(Direction dir)
+{
+    direction_ = dir;
+}
+
+int Snake::getLength() const
+{
+    return length_;
+}
+
+void Snake::draw(class Matrix32 &gfx, Color333 color) const
+{
+    Node *current = head_;
+    while (current)
+    {
+        gfx.setSafe(current->x, current->y, color);
+        current = current->next;
+    }
+}
+
+SnakeScene::SnakeScene() : snake_(Snake::kInitialLength - 1 + Helpers::random(MATRIX_WIDTH - Snake::kInitialLength),
+                                  Snake::kInitialLength - 1 + Helpers::random(MATRIX_HEIGHT - Snake::kInitialLength)) // Randomly place snake
+{
+}
 
 void SnakeScene::setup(AppContext &ctx)
 {
@@ -6,4 +70,6 @@ void SnakeScene::setup(AppContext &ctx)
 
 void SnakeScene::loop(AppContext &ctx)
 {
+    // Draw snake
+    snake_.draw(ctx.gfx, Colors::Muted::Green);
 }
--- GRID/SnakeScene.h
+++ GRID/SnakeScene.h
@@ -2,12 +2,52 @@
 #define SNAKE_SCENE_H
 
 #include "Scene.h"
-#include <bitset>
+#include "Colors.h"
 
+class Snake
+{
+    struct Node
+    {
+        int x;
+        int y;
+        Node *next;
+        Node(int x, int y) : x(x), y(y), next(nullptr) {}
+    };
+
+    Node *head_;
+    Node *tail_;
+    int length_;
+    enum Direction
+    {
+        Up,
+        Down,
+        Left,
+        Right
+    } direction_;
+
+public:
+    static constexpr int kInitialLength = 3;
+
+    Snake(int startX, int startY);
+    ~Snake();
+    // void move();
+    // void grow();
+    // bool checkCollision(int gridWidth, int gridHeight) const;
+    int getHeadX() const;
+    int getHeadY() const;
+    Direction getDirection() const;
+    void setDirection(Direction dir);
+    int getLength() const;
+    void draw(class Matrix32 &gfx, Color333 color) const;
+};
 
 class SnakeScene : public Scene
 {
+    Snake snake_;
+
 public:
+    SnakeScene();
+
     void setup(AppContext &ctx) override;
 
     void loop(AppContext &ctx) override;
